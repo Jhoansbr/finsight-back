8a7300ad2d3e947fdd30710d39602a040a63ee63
Jhoan Blanco <jhoansbr2004@gmail.com>
2025-11-23 10:15:41 -0500

    conexion bd secundaria v2

--- src/routes/analisis.routes.js
+++ src/routes/analisis.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { analisisController } from '../controllers/analisis.controller.js';
-import { authenticate } from '../middlewares/auth.middleware.js';
+import { authenticate } from '../middleware/auth.js';
 
 const router = Router();
 
