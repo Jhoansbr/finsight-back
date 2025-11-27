5e045a5cc405d58aa4c4bf46648557535c6f012d
Jhoan Blanco <jhoansbr2004@gmail.com>
2025-11-26 19:19:33 -0500

    Forgot Password v5

--- src/config/email.js
+++ src/config/email.js
@@ -1,2 +1,18 @@
-console.error('Error en configuración de correo:', error);
+import nodemailer from 'nodemailer';
+
+export const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true para 465, false para otros puertos
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+// Verificar conexión
+transporter.verify().then(() => {
+    console.log('Listo para enviar correos');
+}).catch((error) => {
+    console.error('Error en configuración de correo:', error);
 });
